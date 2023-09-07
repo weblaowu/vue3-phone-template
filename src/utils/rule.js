b7629307aa499c6eaa96dff1db70c99ccb4de759
wuxie <stephen419@126.com>
2023-09-07 15:31:22 +0800

    feat: add test env

--- src/utils/rule.js
+++ src/utils/rule.js
@@ -2,5 +2,4 @@
 export const validateEnAndNum = /^[0-9a-zA-Z]+$/gi
 
 // 手机号正则
-export const regPhone =
-	/^(0|86|17951)?(13[0-9]|15[012356789]|166|17[3678]|18[0-9]|14[57])[0-9]{8}$/
+export const regPhone = /^1[34578]\d{9}$/
