105e04800920d8e89227c17815fc3ccb9e62c5be
wuxie <stephen419@126.com>
2023-10-16 17:23:33 +0800

    fix: 修复Empty组件引用

--- src/components/NoPromission.vue
+++ src/components/NoPromission.vue
@@ -1,11 +1,13 @@
 <template>
-	<Empty :image="getImgUrl('lock')" title="暂无权限" content="很抱歉您无权访问">
-	</Empty>
+	<Empty
+		:image="getImgUrl('lock')"
+		title="暂无权限"
+		content="很抱歉您无权访问"
+	/>
 </template>
 
 <script setup>
 import { getImgUrl } from '@utils/image'
-import Empty from '@/components/Empty/index.vue'
 </script>
 
 <style lang="scss" scoped></style>
